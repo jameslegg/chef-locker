71d9b604dc7d52ad4dbb7ecd44265ea22779a23b
James Legg <james.legg@conversocial.com>
2015-07-14 13:06:36 +0100

    Use the ZK gem's implementation of the zookeeper locking recipe to do locking

--- libraries/clocker.rb
+++ libraries/clocker.rb
@@ -1,50 +1,112 @@
+class Clocker
+  class ZKs
+    def initialize
+      # hash of zookeeper locks we use one connection per lock id
+      @@zkls = {}
+      return
+    end
+
+    # Create or retrieve a locker as appriate
+    def locker(lock, zookeeper = nil)
+      require 'zk'
+      if @@zkls.include?(lock)
+        Chef::Log.debug("Returning locker for #{lock}")
+        @@zkls[lock]
+        #@@zkls[lock].locker(lock)
+      elsif zookeeper
+        Chef::Log.debug("Creating zookeeper connection for #{lock}")
+        zkc = ZK.new(zookeeper)
+        @@zkls[lock] = ZK.new(zookeeper)
+        @@zkls[lock] = zkc.locker(lock)
+      else
+        # Lock neither found nor created
+        Chef::Log.debug("No lock found for #{lock}")
+        false
+      end
+    end
+
+    def disco(lock)
+      Chef::Log.debug("Disconecting #{lock}'s zookeeper connection")
+      if @@zkls.include?(lock)
+         @@zkls[lock].close
+      end
+    end
+  end
+end
+
 class Chef
+  class Recipe::Clocker
+    # We need the run_context passed to us so we can look inside the resource
+    # collection for our clocker resource.
+    def self.held?(lock_id, run_context)
+      # We must dig the clocker resource out of the resource collection.
+      begin
+        cl = run_context.resource_collection.find(clocker: lock_id)
+      rescue Chef::Exceptions::ResourceNotFound
+        # That clocker resource dosn't exist in the resource collection
+        Chef::Log.warn("clocker[#{lock_id}] not found!")
+        return false
+      end
+      # Use the existing ZK::Locker to establish if the lock still exists
+      zkconn = cl.zkconn
+      aclocker = zkconn.locker(lock_id)
+      if aclocker
+        aclocker.assert
+      else
+        aclocker
+      end
+    end
+  end
+
   class Resource::Clocker < Resource
     def initialize(name, run_context = nil)
       super
 
-      @resource_name = :clocker_lock
       @provider = Provider::Clocker
       @action = :clockon
       @allowed_actions.push(:clockon, :clockoff)
+      # Clocker::ZKs with the same lockid are shared between diffrent Resources
+      # this allows them to share a single connection to zookeeper
+      @zkconn = ::Clocker::ZKs.new
     end
 
     def zookeeper(arg = nil)
-      set_or_return(:zookeeper, arg, kind_of: String)
+      set_or_return(:zookeeper, arg, kind_of: String, required: true)
+    end
+
+    def lockid(arg = nil)
+      set_or_return(:lockid, arg, kind_of: String, required: true)
     end
 
     def lockwait(arg = nil)
       set_or_return(:lockwait, arg, kind_of: [ Integer, FalseClass, TrueClass ])
     end
+
+    def zkconn(arg = nil)
+      set_or_return(:zkconn, arg, kind_of: [ Object ])
+    end
+
   end
 
   class Provider::Clocker < Provider
     def load_current_resource
-      require 'zk'
-      @current_resource = Chef::Resource::Clocker.new(@new_resource_name)
-      @current_resource.name(@new_resource.name)
-      @current_resource.zookeeper(@new_resource.zookeeper)
-      @current_resource.lockwait(@new_resource.lockwait)
-      @current_resource
-      # Setup a zk connection and a locker for later use
-      @zkconn = ZK.new(@new_resource.zookeeper)
-      @aclocker = @zkconn.locker(@new_resource.name)
-    end
-
-    def cleanup_after_converge
-      @zkconn.close
+      @name = new_resource.name
+      @lockid = new_resource.lockid
+      @zookeeper = new_resource.zookeeper
+      @lockwait = new_resource.lockwait
+      @zkconn = new_resource.zkconn
+      @aclocker = @zkconn.locker(@lockid,  @zookeeper)
     end
 
     def action_clockon
-      @aclocker.lock({ :wait => @new_resource.lockwait })
-      puts "sleep......................"
-      puts "sleep......................"
-      puts "sleep......................"
-      sleep(10)
+      Chef::Log.info("Taking lock: #{@lockid}")
+      @aclocker.lock({ :wait => @lockwait })
     end
 
     def action_clockoff
+      Chef::Log.info("Releasing lock: #{@lockid}")
       @aclocker.unlock
+      @zkconn.disco(@zookeeper)
     end
   end
 end
--- test/cookbooks/cl_test/recipes/default.rb
+++ test/cookbooks/cl_test/recipes/default.rb
@@ -1,55 +1,31 @@
-# Include clocker to get the zk gem
 include_recipe 'clocker'
-if Clocker::Lock.clockon('clocker_zookeeper_1:2181', 'testlock-A',
-                         node['hostname'])
-  log "CORRECT: Taken Lock A"
-else
-  log "INCORRECT: did not take Lock A"
+
+clocker 'hlock-test' do
+  lockid 'hlock-test'
+  lockwait 10
+  zookeeper 'clocker_zookeeper_1:2181'
+  action :clockon
 end
 
-# try and take the lock again wait 2 seconds, retry 3 times
-if Clocker::Lock.clockon('clocker_zookeeper_1:2181', 'testlock-A',
-                         node['hostname'], 2, 3)
-  log "INCORRECT: Took Lock A again"
-else
-  log "CORRECT: did not take Lock A as it was already taken"
+log 'The lock hlock-test exists and is held' do
+  level :warn
+  only_if { Clocker.held?('hlock-test', run_context) }
 end
 
-if Clocker::Lock.clock?('clocker_zookeeper_1:2181', 'testlock-A')
-  log "CORRECT: Lock A exists"
-else
-  log "INCORRECT: Lock A does not exist"
+log 'Then hlock-fake lock does not exist, this log message will not appear' do
+  level :warn
+  only_if { Clocker.held?('hlock-fake', run_context) }
 end
 
-Clocker::Lock.clockoff('clocker_zookeeper_1:2181', 'testlock-A',
-                       node['hostname'])
-
-if Clocker::Lock.clock?('clocker_zookeeper_1:2181', 'testlock-B')
-  log "INCORRECT: Locklock B exists"
-else
-  log "CORRECT: Lock B does not exist"
+clocker 'hlock-test' do
+  lockid 'hlock-test'
+  lockwait 10
+  zookeeper 'clocker_zookeeper_1:2181'
+  action :clockoff
 end
 
-Clocker::Lock.clockon('clocker_zookeeper_1:2181', 'testlock-C',
-                      'fake-node-hostname')
-
-# Should be unable to remove this lock as it's not the owner
-if Clocker::Lock.clockoff('clocker_zookeeper_1:2181', 'testlock-C',
-                          node['hostname'])
-  log "INCORRECT: Lock C was unlocked, but I'm no the owner"
-else
-  log "CORRECT: Lock C was left locked, because I'm not the owner"
+log 'The lock hlock-test is no longer held, this message will not appear' do
+  level :warn
+  only_if { Clocker.held?('hlock-test', run_context) }
 end
 
-# Force the removal of testlock-C
-if Clocker::Lock.flockoff('clocker_zookeeper_1:2181', 'testlock-C')
-  log "CORRECT: Lock C was forcibly unlocked"
-else
-  log "INCORRECT: Lock C was unable to be forcibly unlocked"
-end
-
-if Clocker::Lock.clock?('clocker_zookeeper_1:2181', 'testlock-C')
-  log "INCORRECT: Lock C exists"
-else
-  log "CORRECT: Lock C does not exist"
-end
